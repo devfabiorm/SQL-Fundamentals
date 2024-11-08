b889fab4ea19571f1179ccca43e2595e264c1a7f
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-11-07 22:55:20 -0300

    first commit

---
+++ Create_And_Drop_Database.sql
@@ -0,0 +1,15 @@
+-- SQL COMMAND TO CREATE A DATABASE
+CREATE DATABASE [Curso]
+
+-- SQL SCRIPT TO KILL PROCESS BEING EXECUTED
+USE [master]
+
+DECLARE @kill varchar(8000) = '';
+SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id)
+FROM sys.dm_exec_sessions
+WHERE database_id = DB_ID('Curso')
+
+EXEC(@kill);
+
+-- SQL COMMAND TO DELETE A DATABASE
+DROP DATABASE [Curso]
