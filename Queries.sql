56f03636e4323064b82ba0cdbd31e7bd94831886
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-11-12 21:57:46 -0300

    feat: created more queries using diferente operator to apply more filters

--- Queries.sql
+++ Queries.sql
@@ -147,3 +147,158 @@ SELECT
     SUM([Id])
 FROM
     [Curso]
+
+-- Getting data starting with Fundamentos
+SELECT
+    [Id], [Nome], [CategoriaId]
+FROM
+    [Curso]
+WHERE [Nome] LIKE 'Fundamentos%'
+
+-- Getting data ending with C#
+SELECT
+    [Id], [Nome], [CategoriaId]
+FROM
+    [Curso]
+WHERE [Nome] LIKE '%C#'
+
+-- Getting data that contains the letter C
+SELECT
+    [Id], [Nome], [CategoriaId]
+FROM
+    [Curso]
+WHERE [Nome] LIKE '%c%'
+
+-- Getting data in a specific list of values (ex: ids 1 and 2 and 3)
+SELECT
+    [Id], [Nome], [CategoriaId]
+FROM
+    [Curso]
+WHERE [Id] IN (1, 2, 3)
+
+-- Getting data between two values
+SELECT
+    [Id], [Nome], [CategoriaId]
+FROM
+    [Curso]
+WHERE [Id] BETWEEN 2 AND 3
+
+-- Getting data applying aliases
+SELECT TOP 100
+    [Id] AS [Codigo],
+    [Nome]
+FROM
+    [Curso]
+
+-- Getting data joining two tables related to each other and retrieveing only data present in the two tables (intersection)
+SELECT TOP 100
+    *
+FROM
+    [Curso]
+    INNER JOIN [Categoria]
+        ON [Curso].[CategoriaId] = [Categoria].[Id]
+
+SELECT TOP 100
+    [Curso].[Id],
+    [Curso].[Nome],
+    [Categoria].[Id] AS [Categoria],
+    [Categoria].[Nome]
+FROM
+    [Curso]
+    INNER JOIN [Categoria]
+        ON [Curso].[CategoriaId] = [Categoria].[Id]
+
+-- Getting data prioritizing first table mentioned in the statement even if it generates null values on result
+SELECT TOP 100
+    [Curso].[Id],
+    [Curso].[Nome],
+    [Categoria].[Id] AS [Categoria],
+    [Categoria].[Nome]
+FROM
+    [Curso]
+    LEFT JOIN [Categoria]
+        ON [Curso].[CategoriaId] = [Categoria].[Id]
+
+
+-- Getting data prioritizing second table mentioned in the statement even if it generates null values on result
+
+SELECT TOP 100
+    [Curso].[Id],
+    [Curso].[Nome],
+    [Categoria].[Id] AS [Categoria],
+    [Categoria].[Nome]
+FROM
+    [Curso]
+    RIGHT JOIN [Categoria]
+        ON [Curso].[CategoriaId] = [Categoria].[Id]
+
+-- Getting all data from the two tables even if they have not related data in the other table involved in the statement
+SELECT TOP 100
+    [Curso].[Id],
+    [Curso].[Nome],
+    [Categoria].[Id] AS [Categoria],
+    [Categoria].[Nome]
+FROM
+    [Curso]
+    FULL OUTER JOIN [Categoria]
+        ON [Curso].[CategoriaId] = [Categoria].[Id]
+
+-- Unifying two table (related or not) in a query (their column must have similar type of data)
+
+SELECT TOP 100
+    [Id], [Nome]
+FROM
+    [Curso]
+
+UNION
+
+SELECT TOP 100
+    [Id], [Nome]
+FROM
+    [Categoria]
+
+-- Unifying two table (related or not) in a query avoing duplicated data
+
+SELECT TOP 100
+    [Id], [Nome]
+FROM
+    [Curso]
+
+UNION ALL
+
+SELECT TOP 100
+    [Id], [Nome]
+FROM
+    [Categoria]
+
+SELECT TOP 100
+    *
+FROM
+    [Categoria]
+    INNER JOIN [Curso] ON [Curso].[CategoriaId] = [Categoria].[Id]
+
+-- Querying by grouping data to get the number of courses in each category
+
+SELECT TOP 100
+    [Categoria].[Nome],
+    COUNT([Curso].[CategoriaId]) AS [Cursos]
+FROM
+    [Categoria]
+    INNER JOIN [Curso] ON [Curso].[CategoriaId] = [Categoria].[Id]
+GROUP BY
+    [Categoria].[Nome],
+    [Curso].[CategoriaId]
+
+-- Querying by grouping data to get the number of courses in each category where the number of categories must be greater tha 1
+
+SELECT TOP 100
+    [Categoria].[Nome],
+    COUNT([Curso].[CategoriaId]) AS [Cursos]
+FROM
+    [Categoria]
+    INNER JOIN [Curso] ON [Curso].[CategoriaId] = [Categoria].[Id]
+GROUP BY
+    [Categoria].[Nome],
+    [Curso].[CategoriaId]
+HAVING
+    COUNT([Curso].[CategoriaId]) > 1
