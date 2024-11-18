8cf5b11a155c0d9617e7c0ecc232f85dabe87347
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-11-17 23:05:11 -0300

    feat: strarting a new database and tables

---
+++ Create_Database_And_Tables.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE [balta]
+
+CREATE TABLE [Studdent] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR(120) NOT NULL,
+    [Email] NVARCHAR(180) NOT NULL,
+    [Document] NVARCHAR(20) NULL,
+    [Phone] NVARCHAR(20) NULL,
+    [CreateDate] DATETIME NOT NULL DEFAULT(GETDATE()),
+    CONSTRAINT [PK_Student] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Author] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR(80) NOT NULL,
+    [Title] NVARCHAR(80) NOT NULL,
+    [Image] NVARCHAR(1024) NOT NULL,
+    [Bio] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR(450) NULL,
+    [Email] NVARCHAR(160) NOT NULL,
+    [Type] TINYINT NOT NULL, -- 0 ~ 255
+    CONSTRAINT [PK_Author] PRIMARY KEY ([Id])
+)
+GO
+
+CREATE TABLE [Career] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [DurationMinutes] INT NOT NULL,
+    [Active] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    CONSTRAINT [PK_Career] PRIMARY KEY ([Id])
+)
+GO
+
+CREATE TABLE [Category] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Order] INT NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Featured] BIT NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+)
+GO
