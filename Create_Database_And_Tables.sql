e9553e00ded55e1d05ddb0e29e5b7d2ba336b24b
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-11-18 23:01:38 -0300

    feat: finsihed tables creation

--- Create_Database_And_Tables.sql
+++ Create_Database_And_Tables.sql
@@ -1,6 +1,10 @@
 CREATE DATABASE [balta]
+GO
 
-CREATE TABLE [Studdent] (
+USE [balta]
+GO
+
+CREATE TABLE [Student] (
     [Id] UNIQUEIDENTIFIER NOT NULL,
     [Name] NVARCHAR(120) NOT NULL,
     [Email] NVARCHAR(180) NOT NULL,
@@ -34,7 +38,7 @@ CREATE TABLE [Career] (
     [Featured] BIT NOT NULL,
     [Tags] NVARCHAR(160) NOT NULL,
     CONSTRAINT [PK_Career] PRIMARY KEY ([Id])
-)
+);
 GO
 
 CREATE TABLE [Category] (
@@ -46,5 +50,52 @@ CREATE TABLE [Category] (
     [Description] TEXT NOT NULL,
     [Featured] BIT NOT NULL,
     CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
-)
+);
+GO
+
+CREATE TABLE [Course] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Tag] NVARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [Level] TINYINT NOT NULL,
+    [DurationMinutes] INT NOT NULL,
+    [CreateDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NOT NULL,
+    [Active] BIT NOT NULL,
+    [Free] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [AuthorId] UNIQUEIDENTIFIER NOT NULL,
+    [CategoryId] UNIQUEIDENTIFIER NOT NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Course_Author_AuthorId] FOREIGN KEY ([AuthorId]) REFERENCES [Author] ([Id]) ON DELETE NO ACTION, -- ON DELETE CASCADE -- SQL COMMAND TO DELETE RELATED REGISTER
+    CONSTRAINT [FK_Course_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE NO ACTION
+);
+GO
+
+CREATE TABLE [CareerItem] (
+    [CareerId] UNIQUEIDENTIFIER NOT NULL,
+    [CourseId] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Order] TINYINT NOT NULL,
+    CONSTRAINT [PK_CareerItem] PRIMARY KEY ([CourseId], [CareerId]),
+    CONSTRAINT [FK_CareerItem_Career_CareerId] FOREIGN KEY ([CareerId]) REFERENCES [Career] ([Id]),
+    CONSTRAINT [FK_CareerItem_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id])
+);
+GO
+
+CREATE TABLE [StudentCourse] (
+    [CourseId] UNIQUEIDENTIFIER NOT NULL,
+    [StudentId] UNIQUEIDENTIFIER NOT NULL,
+    [Progress] TINYINT NOT NULL,
+    [Favorite] BIT NOT NULL,
+    [StartDate] DATETIME NOT NULL DEFAULT(GETDATE()),
+    [LastUpdateDate] DATETIME NULL,
+    CONSTRAINT [PK_StudentCourse] PRIMARY KEY ([CourseId], [StudentId]),
+    CONSTRAINT [FK_StudentCourse_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id]),
+    CONSTRAINT [FK_StudentCourse_Student_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Student] ([Id])
+);
 GO
